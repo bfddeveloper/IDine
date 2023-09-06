e4af2832f8d908d85651c1c10bdf65e0a3a86c11
bfddeveloper <bdicksondev@gmail.com>
2023-09-06 09:33:36 -0500

    Initial Commit

---
+++ IDine/IDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDineApp.swift
+//  IDine
+//
+//  Created by Brody Dickson on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct IDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
